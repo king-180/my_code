1b487446bb2e99e555b28dbb48135beaf89a15b9
SC4442 <xing.wang@wanbangauto.com>
2021-04-16 16:53:16 +0800

    全局异常，枚举，mybatis

--- mybatis_demo/src/main/java/com/wx/demo/controller/BookController.java
+++ mybatis_demo/src/main/java/com/wx/demo/controller/BookController.java
@@ -2,8 +2,11 @@ package com.wx.demo.controller;
 
 import com.wx.demo.entity.Book;
 import com.wx.demo.service.BookService;
+import com.wx.demo.validator.anno.group.AddGroup;
+import com.wx.demo.validator.anno.group.UpdateGroup;
 import com.wx.demo.vo.ResultVO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,16 @@ public class BookController {
     public ResultVO<List<Book>> list(@RequestBody Book book) {
         List<Book> bookList = bookService.list(book);
         return new ResultVO<>(bookList);
+    }
+
+    public ResultVO<Void> add(@Validated(value = AddGroup.class) @RequestBody Book book) {
+
+        return new ResultVO<>();
+    }
+
+    public ResultVO<Void> update(@Validated(value = UpdateGroup.class) @RequestBody Book book) {
+
+        return new ResultVO<>();
     }
 
 }
