93cc2ba5a40f1cd18c59397bbf07f2448f8d6ca5
wangxing <xing.wang@wanbangauto.com>
2021-06-30 17:05:39 +0800

    csv、txt文件读取2

--- java8_demo/src/main/java/file/ReadTxtDemo.java
+++ java8_demo/src/main/java/file/ReadTxtDemo.java
@@ -10,6 +10,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
@@ -18,49 +19,91 @@ import java.util.stream.Collectors;
  */
 public class ReadTxtDemo {
 
+    static String res = "";
+    static String res2 = "";
+
     public static void main(String[] args) throws IOException {
 
         String path1 = "D:\\downDD\\电站运营方\\charging_station_data.txt";
         String path2 = "D:\\downDD\\电站运营方\\t_company2.txt";
         String path3 = "D:\\downDD\\电站运营方\\t_company_type_20210628.csv";
 
-        List<ChargingData> txt1 = new ArrayList<>(420000);
-        List<Company> txt2 = new ArrayList<>(180000);
-        List<CompanyType> txt3 = new ArrayList<>(160000);
+        List<ChargingData> chargingDataTxt = new ArrayList<>(420000);
+        List<Company> companyTxt = new ArrayList<>(180000);
+        List<CompanyType> companyTypeTxt = new ArrayList<>(160000);
 
-        readChargingDataToList(path1, txt1);
-        readCompanyToList(path2, txt2);
-        readCompanyTypeToList(path3, txt3);
+        readChargingDataToList(path1, chargingDataTxt);
+        readCompanyToList(path2, companyTxt);
+        readCompanyTypeToList(path3, companyTypeTxt);
 
-        // 公司类型为 1 的所有companyId
-        List<String> companyIds = txt3.stream()
+        // 公司类型为 1 子公司 的所有 companyId
+        List<String> companyIds = companyTypeTxt.stream()
                 .filter(companyType -> "1".equals(companyType.getType()))
                 .map(CompanyType::getCompanyId).collect(Collectors.toList());
 
-        // 所有 公司类型为 1 的 orgCode
-        List<String> orgCodes = txt2.stream().filter(company -> {
+        System.out.println("子公司的 companyId :" + companyIds);
+        System.out.println("子公司的 companyId 个数 :" + companyIds.size());
+
+        // 所有 公司类型为 1 子公司 的 orgCode
+        List<String> orgCodes = companyTxt.stream().filter(company -> {
             String companyId = company.getCompanyId();
             return companyIds.contains(companyId);
         }).map(Company::getOrgCode).collect(Collectors.toList());
 
-        // flags 索引为 8对应的数字是 2的所有数据 6800条
-        List<ChargingData> chargingDataList = txt1.stream().filter((chargingData -> {
-            String flags = chargingData.getFlags();
-            if (flags.length() > 8) {
-                char flag = flags.charAt(8);
-                return flag == '2';
-            }
-            return false;
-        })).filter(chargingData -> {
+        System.out.println("子公司的 orgCode ：" + orgCodes);
+        System.out.println("子公司的 orgCode 个数：" + orgCodes.size());
+
+//        // flags 索引为 8 对应的数字是 2 的所有数据 6800条
+//        List<ChargingData> chargingDataList = chargingDataTxt.stream().filter((chargingData -> {
+//            String flags = chargingData.getFlags();
+//            if (flags.length() > 8) {
+//                char flag = flags.charAt(8);
+//                return flag == '2';
+//            }
+//            return false;
+//        })).filter(chargingData -> {
+//            String assetOperator = chargingData.getAssetOperator();
+//            if (orgCodes.contains(assetOperator)) {
+//                return true;
+//            }
+//            System.out.println(chargingData.getStationId());
+//            res += ", " + chargingData.getStationId();
+//            return false;
+//        }).collect(Collectors.toList());
+//        res += "'";
+//
+//        System.out.println(chargingDataList.size());
+//        System.out.println(res.replaceFirst(", ", ""));
+
+
+        // 资产运营方为子公司 但是 FLAGS 第8位不是2
+        AtomicInteger cnt = new AtomicInteger();
+        AtomicInteger cnt2 = new AtomicInteger();
+        List<ChargingData> chargingDataList1 = chargingDataTxt.stream().filter(chargingData -> {
             String assetOperator = chargingData.getAssetOperator();
             if (orgCodes.contains(assetOperator)) {
+                cnt.getAndIncrement();
+                return true;
+            }
+            return false;
+        }).filter((chargingData -> {
+            String flags = chargingData.getFlags();
+            if (flags.length() > 8 && flags.charAt(8) == '2') {
                 return true;
             }
             System.out.println(chargingData);
+            res2 += "', '" + chargingData.getStationId();
+            cnt2.getAndIncrement();
             return false;
-        }).collect(Collectors.toList());
+        })).collect(Collectors.toList());
+        res2 += "'";
 
-        System.out.println(chargingDataList.size());
+//        System.out.println(chargingDataList1.size());
+        System.out.println("chargingDataTxt 中子公司的个数：" + cnt.get());
+        System.out.println("chargingDataTxt 中是子公司但是第8位不是2的个数：" + cnt2.get());
+        System.out.println(res2.replaceFirst("', '", "'"));
+
+
 
     }
 
